93bed6bc210cea8f3f786a67dd690751f72bb158
kanvar22 <league@WTS10.attlocal.net>
2018-11-08 19:57:40 -0800

    Finished everything except for Snake

--- src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
+++ src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
@@ -10,8 +10,31 @@ public class CustomButtonOptionPanes {
 
 		int input = JOptionPane.showOptionDialog(null, "Choose a day of the week", "Custom Buttons", 0, -1, null,
 				options, 0);
-
-		String choice = options[input];
+			switch(input) {
+			case(0):
+				System.out.println("Its Sunday!");
+			break;
+			case(1):
+				System.out.println("Its Monday!");
+			break;
+			case(2):
+				System.out.println("Its Tuesday!");
+			break;
+			case(3):
+				System.out.println("Its Wednessday!");
+			break;
+			case(4):
+				System.out.println("Its Thursday!");
+			break;
+			case(5):
+				System.out.println("Its Friday!");
+			break;
+			case(6):
+				System.out.println("Its Saturday!");
+			break;
+			}
+			
+		
 		
 		//use a switch statement to do something cool for each option
 		
--- src/_05_Enum_Stuff/_00_Horoscope.java
+++ src/_05_Enum_Stuff/_00_Horoscope.java
@@ -8,7 +8,73 @@ public class _00_Horoscope {
 	
 	// 2. Write a method that takes in a Zodiac enum object and uses a JOPtionPane to display
 	//    a different horoscope based on the Zodiac's state.
+public static void main(String[] args) {
+	horoscope(Zodiac.ARIES);
+	horoscope(Zodiac.TAURUS);
+	horoscope(Zodiac.GEMINI);
+	horoscope(Zodiac.CANCER);
+	horoscope(Zodiac.LEO);
+	horoscope(Zodiac.LIBRA);
+	horoscope(Zodiac.SCORPIO);
+	horoscope(Zodiac.SAGITTARIUS);
+	horoscope(Zodiac.CAPRICORN);
+	horoscope(Zodiac.AQUARIUS);
+	horoscope(Zodiac.PISCES);
+	horoscope(Zodiac.VIRGO);
+}
 	
+	static void horoscope (Zodiac z) {
+		switch(z) {
+			case ARIES:{
+				JOptionPane.showMessageDialog(null, "You will die early");
+				break;
+			}
+			case TAURUS:{
+				JOptionPane.showMessageDialog(null, "You will get married at the age 16");
+				break;
+			}
+			case GEMINI:{
+				JOptionPane.showMessageDialog(null, "You will get married at the age 25");
+				break;
+			}
+			case CANCER:{
+				JOptionPane.showMessageDialog(null, "You will have 5 kids");
+				break;
+			}
+			case LEO:{
+				JOptionPane.showMessageDialog(null, "You will die at an early age");
+				break;
+			}
+			case VIRGO:{
+				JOptionPane.showMessageDialog(null, "You will be sucsessful");
+				break;
+			}
+			case LIBRA:{
+				JOptionPane.showMessageDialog(null, "You will become a billionaire");
+				break;
+			}
+			case SCORPIO:{
+				JOptionPane.showMessageDialog(null, "You will have back luch on your next test");
+				break;
+			}
+			case SAGITTARIUS:{
+				JOptionPane.showMessageDialog(null, "You will never get into college");
+				break;
+			}
+			case CAPRICORN:{
+				JOptionPane.showMessageDialog(null, "You will get onto the Golden State Warriors basktball team");
+				break;
+			}
+			case AQUARIUS:{
+				JOptionPane.showMessageDialog(null, "You will drink hot chocolate some time in your future");
+				break;
+			}
+			case PISCES:{
+				JOptionPane.showMessageDialog(null, "You will go to Chipotle in 2 days");
+				break;
+			}
+	}
+	}
 	// 3. Make a main method to test your method
 	
 }
